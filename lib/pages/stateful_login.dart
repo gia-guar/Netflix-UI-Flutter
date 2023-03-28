43d821610d9297d5c7d70749591a4e6410b2c75a
Gianmarco Guarnier <gianmarco.guarnier@hotmail.com>
2023-03-28 19:58:25 +0200

    ui modes finished

--- lib/pages/stateful_login.dart
+++ lib/pages/stateful_login.dart
@@ -27,7 +27,8 @@ class _ThemedLoginPageState extends State<ThemedLoginPage>{
   // ignore: non_constant_identifier_names
   static double PADDING_FROM_BORDERS  = 20;
 
-  bool _isDark=true;
+  bool _isDark=true; 
+  Icon _themeIcon = Icon(Icons.mode_night_rounded, color: Colors.white,);
   Color _primaryColor1 =Colors.black;
   Color _primaryColor2 = Colors.white;
   Color _secondaryColor1 = Colors.white10;
@@ -57,12 +58,14 @@ class _ThemedLoginPageState extends State<ThemedLoginPage>{
         _secondaryColor1 = Colors.white10;
         _accentColor = Colors.white70;
         _focusAccentColor = Colors.white;
+        _themeIcon = const Icon(Icons.mode_night, color: Colors.black,);
       }else{
         _primaryColor1 =Colors.white;
         _primaryColor2 = Colors.black;
         _secondaryColor1 = Colors.black12;
         _accentColor = Colors.grey;
         _focusAccentColor = Colors.black;
+        _themeIcon = const Icon(Icons.wb_sunny_rounded, color: Colors.white);
       }
     });
   }
@@ -82,112 +85,121 @@ class _ThemedLoginPageState extends State<ThemedLoginPage>{
 
     return Scaffold(
       backgroundColor: _primaryColor1,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
         children: [
-        
-        FloatingActionButton(onPressed: ()=> _changeTheme(),
-          backgroundColor: Colors.redAccent,
-        ),
-
-        // logo
-        // const Center(child: Icon(Icons.lock, size: 100, color: Colors.white70,)),
-        const SizedBox(height: 10),
-        Image.asset('lib/logos/netflix.png', height: 100,),
-        
-        // welcome back
-        Text(
-          'Welcome back! we missed you!',
-          style: TextStyle(
-            color: _primaryColor2,
-            fontSize: 16,
-          ),
-        ),
-
-        const SizedBox(height: 25),
-
-        // username text field
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: PADDING_FROM_BORDERS),
-          child: my_textfield(
-            controller:usernameController,
-            obscureText: false, 
-            hintText: 'username',
-            theme: [
-              _secondaryColor1,
-              _primaryColor2,
-              _accentColor,
-              _focusAccentColor
-              ,]
-            ),
-        ),
-
-        const SizedBox(height: 25),
-        
-        // password
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: PADDING_FROM_BORDERS),
-          child: my_textfield(
-            controller: passwordController,
-            obscureText: true,
-            hintText: 'password',
-            theme:[
-               _secondaryColor1,
-              _primaryColor2,
-              _accentColor,
-              _focusAccentColor
-              ,]
-          ),
-        ),
-
-        const SizedBox(height:7),
-
-        // forgot password
-        Padding(
-          padding: EdgeInsets.only(right: PADDING_FROM_BORDERS),
-          child: Row(mainAxisAlignment: MainAxisAlignment.end,
+          Row( mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text('Forgot password?',style: TextStyle(color: _primaryColor2),),
+              FloatingActionButton(onPressed: ()=> _changeTheme(),
+                  backgroundColor: _primaryColor2,
+                  child: _themeIcon,
+                ),
             ],
           ),
-        ),
 
-        const SizedBox(height: 10),
+          Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            
+            // logo
+            // const Center(child: Icon(Icons.lock, size: 100, color: Colors.white70,)),
+            const SizedBox(height: 10),
+            Image.asset('lib/logos/netflix.png', height: 100,),
+            
+            // welcome back
+            Text(
+              'Welcome back! we missed you!',
+              style: TextStyle(
+                color: _primaryColor2,
+                fontSize: 16,
+              ),
+            ),
 
-        // signin
-        my_animated_button(text:'SIGN IN', 
-          onPress: signUserIn, 
-          animatedOn: AnimatedOn.onHover,
-          textColor: _primaryColor2,
-          backgroundColor: _primaryColor1,
-          selectedBackgroundColor: _primaryColor2,
-          selectedTextColor: _primaryColor1,
-          borderColor: _primaryColor2,
-        ),
+            const SizedBox(height: 25),
 
-        const SizedBox(height: 10,),
+            // username text field
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: PADDING_FROM_BORDERS),
+              child: my_textfield(
+                controller:usernameController,
+                obscureText: false, 
+                hintText: 'username',
+                theme: [
+                  _secondaryColor1,
+                  _primaryColor2,
+                  _accentColor,
+                  _focusAccentColor
+                  ,]
+                ),
+            ),
 
-        // or continue with
-        or_continue_with_div(pad: PADDING_FROM_BORDERS,), 
-       
-      const SizedBox(height:10),
+            const SizedBox(height: 25),
+            
+            // password
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: PADDING_FROM_BORDERS),
+              child: my_textfield(
+                controller: passwordController,
+                obscureText: true,
+                hintText: 'password',
+                theme:[
+                   _secondaryColor1,
+                  _primaryColor2,
+                  _accentColor,
+                  _focusAccentColor
+                  ,]
+              ),
+            ),
 
-      //continue with google or Apple
-      Row(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //Google
-          SquareTile(path: 'lib/logos/google.png', backgroundColor: _primaryColor2,),
+            const SizedBox(height:7),
 
-          const SizedBox(width: 20,),
+            // forgot password
+            Padding(
+              padding: EdgeInsets.only(right: PADDING_FROM_BORDERS),
+              child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Forgot password?',style: TextStyle(color: _primaryColor2),),
+                ],
+              ),
+            ),
 
-          //Apple
-          SquareTile(path: apple_logo, backgroundColor: _primaryColor2,)
-        ],),
+            const SizedBox(height: 10),
 
-        const SizedBox(height: 20),
+            // signin
+            my_animated_button(text:'SIGN IN', 
+              onPress: signUserIn, 
+              animatedOn: AnimatedOn.onHover,
+              textColor: _primaryColor2,
+              backgroundColor: _primaryColor1,
+              selectedBackgroundColor: _primaryColor2,
+              selectedTextColor: _primaryColor1,
+              borderColor: _primaryColor2,
+            ),
 
-        // not a member? register
-        const Text('not a member? Sign up', style: TextStyle(color: Color.fromARGB(255, 49, 169, 169)), )
-      ],),
+            const SizedBox(height: 10,),
+
+            // or continue with
+            or_continue_with_div(pad: PADDING_FROM_BORDERS,), 
+           
+          const SizedBox(height:10),
+
+          //continue with google or Apple
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Google
+              SquareTile(path: 'lib/logos/google.png', backgroundColor: _primaryColor2,),
+
+              const SizedBox(width: 20,),
+
+              //Apple
+              SquareTile(path: apple_logo, backgroundColor: _primaryColor2,)
+            ],),
+
+            const SizedBox(height: 20),
+
+            // not a member? register
+            const Text('not a member? Sign up', style: TextStyle(color: Color.fromARGB(255, 49, 169, 169)), )
+          ],),
+        ],
+      ),
     );
   }
 }
